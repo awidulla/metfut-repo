c1d16aa07d4a6be23d04be45beb4fae8a98d3fda
awidulla <awidulla@smail.uni-koeln.de>
2024-07-03 09:33:39 +0200

    New class for loss function. All classes registerd, can now be saved and loaded

--- CNN_architecture.py
+++ CNN_architecture.py
@@ -10,6 +10,7 @@ https://github.com/pangeo-data/WeatherBench/blob/master/src/train_nn.py
 import numpy as np
 import matplotlib.pyplot as plt
 import xarray as xr
+import time
 import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras.layers import *
@@ -19,7 +20,7 @@ import pickle
 #from src.score import *
 from collections import OrderedDict
 
-
+@keras.utils.register_keras_serializable()
 class PeriodicPadding2D(tf.keras.layers.Layer):
     '''
     source: WeatherBench
@@ -42,7 +43,7 @@ class PeriodicPadding2D(tf.keras.layers.Layer):
         config.update({'pad_width': self.pad_width})
         return config
 
-
+@keras.utils.register_keras_serializable()
 class PeriodicConv2D(tf.keras.layers.Layer):
     '''
     source: WeatherBench
@@ -58,6 +59,7 @@ class PeriodicConv2D(tf.keras.layers.Layer):
         if type(kernel_size) is not int:
             assert kernel_size[0] == kernel_size[1], 'PeriodicConv2D only works for square kernels'
             kernel_size = kernel_size[0]
+        # kernel_size needs to be uneven!!!! - otherwise size decreases
         pad_width = (kernel_size - 1) // 2
         self.padding = PeriodicPadding2D(pad_width)
         self.conv = Conv2D(
@@ -90,12 +92,14 @@ def build_cnn(filters, kernels, input_shape, dr=0):
 Everything below written by myself
 '''
 
-def initialize_model(data, N_layers, N_filters, Kernel_size):
+def initialize_model(data, N_layers, Kernel_size, N_filters):
     
     # create input variables for build_cnn
     filters     = [N_filters]*(N_layers+1)
     filters[-1] = data.output_filters
     kernels     = [Kernel_size]*(N_layers+1)
+    
+    print(filters, kernels, data.input_shape)
     
     # build and compile
     model = build_cnn(filters, kernels, data.input_shape)
@@ -103,13 +107,14 @@ def initialize_model(data, N_layers, N_filters, Kernel_size):
     
     return model
 
-def initialize_documentation(data):
+def initialize_docu_model(data):
     
     # create dictionary
     documentation = {}
     
     documentation['variables'] = data.output_variables
     documentation['training']  = []
+    documentation['time']      = []
     
     documentation['RMSE']      = {}
     documentation['RMSE']['train'] = []
@@ -122,6 +127,9 @@ def initialize_documentation(data):
     return documentation
 
 def training_cycle(model, data, N_epochs, documentation):
+    
+    # time keeping for training cycle
+    t_start = time.time()
     
     # train model for given number of epochs
     model.fit(data.X_train, data.Y_train, batch_size=128, epochs=N_epochs)
@@ -144,6 +152,73 @@ def training_cycle(model, data, N_epochs, documentation):
         documentation['RMSE']['train'][v].append(rmse_train)
         documentation['RMSE']['valid'][v].append(rmse_valid)
     
+    # save time keeping
+    documentation['time'].append(time.time()-t_start)
+
+def hyper_training(data):
+    '''
+    Initialize and train a model for all variations of hyper parameters
+    All models are saved, training process is saved in dictionary
+    '''
+    
+    # create dictionary for documenting all relevant parameters
+    docu = {}
+    docu['model_names'] = []
+    
+    docu['hyper'] = {}
+    docu['hyper']['layers']  = []
+    docu['hyper']['kernels'] = []
+    docu['hyper']['filters'] = []
+    
+    # iterate all hyperparameters
+    for l in data.layers:
+        for k in data.kernels:
+            for f in data.filters:
+                
+                # skip the execution of first three models - memory problem
+                if l==3 and k==3:
+                    continue
+                
+                if l==3 and k==5 and f==32:
+                    continue
+                
+                # creating distinct name coding
+                model_name = data.case_type+'_s'+str(data.lead_steps)+'_l'+\
+                             str(l)+'_k'+str(k)+'_f'+str(f)
+                
+                print(model_name)
+                docu['model_names'].append(model_name)
+                docu['hyper']['layers'].append(l)
+                docu['hyper']['kernels'].append(k)
+                docu['hyper']['filters'].append(f)
+                
+                # initialize model
+                model = initialize_model(data, l, k, f)
+                model.summary()
+                
+                # initialize documentation
+                docu[str(model_name)] = initialize_docu_model(data)
+                
+                # run training cycles
+                for c in range(data.cycles):
+                    training_cycle(model, data, data.epochs, docu[str(model_name)])
+                    
+                # save model
+                model.save(data.rootdir+'Model/'+model_name+'.keras')
+    
+                # save training for each model (in case server breaks)
+                with open(data.rootdir+'Model/'+model_name+'.pickle', 'wb') as file:
+                    pickle.dump(docu[str(model_name)], file)
+                
+                # clear RAM
+                K.clear_session()
+    
+    # save training for all models
+    with open(data.rootdir+'Model/'+data.case_type+'_s'+str(data.lead_steps)
+              +'.pickle', 'wb') as file:
+        pickle.dump(docu[str(model_name)], file)
+    
+
 def main():
     root_era5 = '/home/anton/Documents/MSPEA/SoSe24/MachineLearning/data_retrieval/2024METFUT_project_data/ERA5/'
     data_ocean = data_prep(root_era5, 5)
@@ -153,11 +228,46 @@ def main():
     documentation = initialize_documentation(data_ocean)
     return data_ocean, model, documentation
 
+def import_model(path, data):
+    # load
+    model = keras.models.load_model(path)
+    
+    # recompile
+    model.compile(keras.optimizers.Adam(1e-4), loss=data.custom_loss)
+
+    return model 
+
+@keras.utils.register_keras_serializable()
+class CustomLoss(keras.losses.Loss):
+    def __init__(self, surface_mask, reduction=tf.keras.losses.Reduction.AUTO, name='custom_loss'):
+        super().__init__(reduction=reduction, name=name)
+        self.surface_mask = surface_mask
+
+    def call(self, y_true, y_pred):
+        # ensure surface_mask has the same shape as y_true and y_pred
+        #self.surface_mask = tf.broadcast_to(self.surface_mask, tf.shape(y_true))
+        
+        # apply surface mask to data
+        y_true = y_true*self.surface_mask
+        y_pred = y_pred*self.surface_mask
+        
+        # compute loss
+        loss = K.sqrt(K.mean(K.square(y_true-y_pred)))
+        
+        return loss        
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({'surface_mask': self.surface_mask})
+        return config
+
+
+
 class data_prep:
     def __init__(self, rootdir, lead_steps):
-        self.rootdir      = rootdir
+        self.rootdir      = rootdir   # path of project data folder 
         self.lead_steps   = lead_steps
-        self.modeltype    = None
+        self.case_type    = None
         
         # default time periods used by Weatherbench 
         self.train_years  = slice('1979', '2015')
@@ -172,7 +282,7 @@ class data_prep:
         self.X_test  = None
         self.Y_test  = None
         
-        
+        # parameters that might be of interest
         self.input_shape      = None
         self.output_filters   = None
         
@@ -188,6 +298,18 @@ class data_prep:
         
         # mask relevant for the loss function
         self.surface_mask = None
+        self.custom_loss  = None
+        
+        # set hyperparameters that will be iterated
+        self.layers  = [3,4,5]
+        # kernels need to be uneven
+        self.kernels = [3,5,7] 
+        self.filters = [32,64,128]
+        
+        # set number of epochs per training cycle and number of cycles
+        self.epochs = 5
+        self.cycles = 30
+        
         
     def change_time(self, begin_train, begin_valid, begin_test, end_test):
         '''
@@ -197,17 +319,14 @@ class data_prep:
         self.valid_years  = slice(str(begin_valid), str(begin_test))
         self.test_years   = slice(str(begin_test),  str(end_test))
     
-    def ocean(self):
+    def import_ocean(self):
         '''
-        Function that imports and set up all relevant data for an ocean model
+        Import ocean fields and replace nan values with climatology
         '''
-        # document that ocean was called
-        self.modeltype = 'Ocean'
-        
         # load relevant datasets
-        sst    = xr.open_mfdataset(self.rootdir+'sst_5.625deg/*.nc',
+        sst    = xr.open_mfdataset(self.rootdir+'ERA5/sst_5.625deg/*.nc',
                                    combine='by_coords').load()
-        siconc = xr.open_mfdataset(self.rootdir+'siconc_5.625deg/*.nc',
+        siconc = xr.open_mfdataset(self.rootdir+'ERA5/siconc_5.625deg/*.nc',
                                    combine='by_coords').load()
         
         # compute climatology
@@ -218,6 +337,31 @@ class data_prep:
         sst = sst.fillna(sst_clim.sel(dayofyear=sst.time.dt.dayofyear))
         siconc = siconc.fillna(siconc_clim.sel(dayofyear=siconc.time.dt.dayofyear))
         
+        return sst, siconc
+    
+    '''
+    write functions
+    import_atmosphere
+    import_surface
+    
+    import_masks # call them sea, land, all
+    '''
+    # Naming
+    # Output_Input 
+    # O - Ocean
+    # L - Land
+    # A - Atmosphere
+    
+    def O_O(self):
+        '''
+        Function that imports and sets up all relevant data for an ocean model
+        '''
+        # document that ocean was called
+        self.case_type = 'O_O'
+        
+        # import data
+        sst, siconc = self.import_ocean()
+        
         # create datasets
         self.get_input_dataset([sst, siconc], ['sst', 'siconc'])
         self.get_output_dataset([sst, siconc], ['sst', 'siconc'])
@@ -226,11 +370,11 @@ class data_prep:
         self.output_variables = ['sst', 'siconc']
         
         # load land sea mask and create a sea-land mask
-        lsmask = xr.open_dataset(self.rootdir+'lsm_5.625deg.nc').load()
+        lsmask = xr.open_dataset(self.rootdir+'ERA5/lsm_5.625deg.nc').load()
         slm = xr.where(lsmask>0.5,0,1)['lsm'].values[0,:,:]
         
         self.surface_mask = K.constant(np.stack([slm, slm],axis=2))
-        
+        self.custom_loss  = CustomLoss(self.surface_mask)
     
     # Maybe split this into function that creates input and output data
     def get_input_dataset(self, datasets, variables):
@@ -307,13 +451,15 @@ class data_prep:
         # information for shape of output
         self.output_filters = self.Y_train.shape[-1]
     
+    '''
     def custom_loss(self, y_true, y_pred):
         # apply surface mask to data
         y_true = y_true*self.surface_mask
         y_pred = y_pred*self.surface_mask
 
         return K.sqrt(K.mean(K.square(y_true-y_pred)))
-
+    '''
+    
 if __name__ == '__main__':
     print('hello')
     #root_era5  = '/home/anton/Documents/MSPEA/SoSe24/MachineLearning/data_retrieval/2024METFUT_project_data/ERA5/'
